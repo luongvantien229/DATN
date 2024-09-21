393093498a02d8b1cbd2fc82be3fdf85a067bb23
LuongVanTien <luongtien234@gmail.com>
2024-09-21 10:40:06 +0700

    end admin

--- frontend/src/pages/user/cart/index.jsx
+++
@@ -1,9 +0,0 @@
-import CartItem from "../../../components/user/cart";
-
-const Cart = () => {
-  return (
-    <CartItem />    
-  );
-};
-
-export default Cart;
--- frontend/src/pages/user/loginAndRegister/index.jsx
+++
@@ -1,9 +0,0 @@
-import LoginAndRegisterItem from "../../../components/user/loginAndRegister";
-
-const LoginAndRegister = () => {
-  return (
-    <LoginAndRegisterItem />    
-  );
-};
-
-export default LoginAndRegister;

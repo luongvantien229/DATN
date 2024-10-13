59844d2727e13b093c55374a4949a8824d5a7745
LuongVanTien <luongtien234@gmail.com>
2024-10-13 11:32:01 +0700

    13/10 env

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 composer install --prefer-dist --no-interaction
 cp .env.example .env
-chmod -R 775 storage
+chmod -R 777 storage
+# php artisan migrate:refresh --seed
 
 # Chạy lệnh php-fpm
 php-fpm
